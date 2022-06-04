53e8fa656a7968af29ec1b2d3752787d4274a24c
antena-kipas <ujankcodet0@gmail.com>
2022-06-04 22:34:54 +0700

    day 2 end, need to break for 1 week for PAT

---
+++ constant/constant.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const firstime string = "adu"
+	const lastname = "du"
+	const angka = 43
+
+	fmt.Println(firstime)
+	fmt.Println(lastname)
+	fmt.Println(angka)
+}
---
+++ constant/multipleconst.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	const (
+		fristname = "adu"
+		lastname  = "du"
+		angka     = 1000
+	)
+
+	fmt.Println(fristname)
+	fmt.Println(lastname)
+	fmt.Println(angka)
+}
