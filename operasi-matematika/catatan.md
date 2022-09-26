c273be228846ffd6369c2dd021968178b15ab8f7
antena-kipas <ujankcodet0@gmail.com>
2022-09-26 13:45:52 +0700

    26 september 2022 operasi matematika

---
+++ operasi-matematika/catatan.md
@@ -0,0 +1,7 @@
+Unary operator 
+
+++      > a = a + 1
+--      > a = a - 1
+-       > negative
++       > positive 
+!       > Boolean kebalikan
--- operasi-matematika/operasi-mtk.go
+++ operasi-matematika/operasi-mtk.go
@@ -3,8 +3,37 @@ package main
 import "fmt"
 
 func main() {
-	var(
-		
-	)
+	var a = 10 
+	var b = 20
+	var c = a + b
+
+	fmt.Println(c)
+	
+	var result = 10 + 100
+	fmt.Println(result)
+
+	a = a + 10
+	fmt.Println(a)
+
+	a += 10
+
+	fmt.Println(a)
+
+
+	a += 10
+	a -= 10
+	a *= 10
+	a /= 10 
+	a %= 10
+
+	var i = 10
+	i++
+	fmt.Println(i)
+
+	var negative = -100
+	var positive = 100
+
+	fmt.Println(negative)
+	fmt.Println(positive)
 }
 
