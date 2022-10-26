11ded3c1ca8e6f66eef88042a83acef19ff8c88d
antena-kipas <ujankcodet0@gmail.com>
2022-10-26 18:28:13 +0700

    else-if

---
+++ if/else-if.go
@@ -0,0 +1,20 @@
+package main
+import "fmt"
+
+func main () {
+	nama := "Jack"
+
+	if nama == "ajax" {
+		fmt.Println("hallo ajax")
+	} else if nama == "joni" {
+		fmt.Println("hallo joni")
+	} else if nama == "drud" {
+		fmt.Println("hallo drud")
+	} else if nama == "Jack" {
+		fmt.Println("Hallo Jack")
+	} else {
+		fmt.Println("hi")
+	}
+
+	
+}
